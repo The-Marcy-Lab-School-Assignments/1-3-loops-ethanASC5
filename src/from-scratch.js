f47d9c82de5f8737dc513597e6502605a8ad5e3f
orwrandal <randypichardo1987@gmail.com>
2024-06-18 10:40:58 -0400

    graded resub

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,4 +1,4 @@
-/** FEEDBACK: Look at the tests, should you include or exclude the 10?  */
+/** FEEDBACK: Great job! However, what was the purpose of putting i as a parameter if i is defined in the for loop?  */
 const loop0UpTo10 = (i) => {
   for (let i = 0; i < 10; i++) {
     console.log(i);
@@ -6,6 +6,7 @@ const loop0UpTo10 = (i) => {
 };
 loop0UpTo10()
 
+/*FEEDBACK: Same feedback here, why is i defined as a parameter?*/
 const loop5to10 = (i) => {
   for (let i = 5; i < 11; i++) {
     console.log(i);
@@ -13,8 +14,6 @@ const loop5to10 = (i) => {
 };
 loop5to10()
 
-// you do NOT need an if check
-/** FEEDBACK: For this one, you do not need to include the 10! How can you change the stopping condition to exclude teh 10? */
 const loopEvenNumbersUpTo10 = () => {
   for (let i = 0 ; i < 10 ; i += 2 ) {
       console.log(i)
@@ -24,7 +23,6 @@ const loopEvenNumbersUpTo10 = () => {
 loopEvenNumbersUpTo10()
 
 
-/** FEEDBACK: We want to be inclusive of the 0 so how should we change the stopping condition to be inclusive? */
 const countdown5to0 = () => {
   for (let i = 5; i >= 0; i--) {
     console.log(i);
